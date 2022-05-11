85eac2281ba3f4f029513c7e29f4c32fdfeecaff
Asmâa a.k.a Kanasayrus <asmaa.e@hiitconsulting.com>
2022-05-11 11:13:56 +0100

    Create sendAlert.php

---
+++ sendAlert.php
@@ -0,0 +1,31 @@
+<?php
+
+require('vendor/autoload.php');
+
+use Vonage\Client\Credentials\Basic;
+use Vonage\Client;
+use Vonage\Verify\Request;
+
+
+$basic = new Basic("411e9028","2W73DtmJqORMjOu9");
+$client = new Client($basic);
+
+
+$request = new Request('212669466359', "Vonage");
+$response = $client->verify()->start($request);
+
+echo "Started verification, `request_id` is " . $response->getRequestId();
+
+
+$result = $client->verify()->check(REQUEST_ID, CODE);
+
+var_dump($result->getResponseData());
+
+try {
+    $result = $client->verify()->cancel(REQUEST_ID);
+    var_dump($result->getResponseData());
+}
+
+catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+}
